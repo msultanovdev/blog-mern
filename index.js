30d9f0ac15d9ffe33602bb2ef6b83f4838300a57
msultanovdev <mukhammadamin.sultanov@gmail.com>
2022-06-22 19:51:57 +0500

    Settin express-validator

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
+import { registerValidator } from './validations/auth.js';
+import { validationResult } from 'express-validator';
 
 mongoose
     .connect('mongodb+srv://admin:root@cluster0.lawtt.mongodb.net/?retryWrites=true&w=majority')
@@ -15,8 +17,15 @@ app.get('/', (req, res) => {
     res.send('Hello');
 })
 
-app.post('/auth/register', (req, res) => {
-    
+app.post('/auth/register', registerValidator, (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json(errors.array());
+    }
+
+    res.json({
+        success: true
+    })
 });
 
 app.listen(4444, (err) => {
