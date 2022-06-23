76098f1b72594b03645853a10078cfa6eccd89d2
msultanovdev <mukhammadamin.sultanov@gmail.com>
2022-06-23 22:37:14 +0500

    .

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import { registerValidator } from './validations/auth.js';
 import { validationResult } from 'express-validator';
+
+import UserModel from './models/User.js';
 
 mongoose
     .connect('mongodb+srv://admin:root@cluster0.lawtt.mongodb.net/?retryWrites=true&w=majority')
@@ -17,15 +20,26 @@ app.get('/', (req, res) => {
     res.send('Hello');
 })
 
-app.post('/auth/register', registerValidator, (req, res) => {
+app.post('/auth/register', registerValidator, async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json(errors.array());
     }
 
-    res.json({
-        success: true
-    })
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+    
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash,
+    });
+
+    const user = await doc.save();
+
+    res.json(user);
 });
 
 app.listen(4444, (err) => {
